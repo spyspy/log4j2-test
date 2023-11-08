b2a33eff8a4279c4441601f687e487a9fa2819eb
spyspy <spyhopping101@gmail.com>
2023-11-09 04:56:42 +0800

    update (work) 2023/11/09 週四 -  4:56:42.78

--- src/main/java/GoLogTest.java
+++ src/main/java/GoLogTest.java
@@ -4,13 +4,13 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Random;
 
-public class Go {
-    private static final Logger logger = LogManager.getLogger(Go.class);
+public class GoLogTest {
+    private static final Logger logger = LogManager.getLogger(GoLogTest.class);
 
     public static void main(String[] args) {
 
         logger.debug("Hello from Log4j 2 - V111 ============================(Start)");
-        String newWord = getRandomWord(1000);
+        String newWord = getRandomWord(10000);
         // in old days, we need to check the log level to increase performance
         /*if (logger.isDebugEnabled()) {
             logger.debug("{}", getNumber());
@@ -35,20 +35,21 @@ public class Go {
         return 5;
     }
 
+    // 產生數字+英文亂碼
     static String getRandomWord(int limit){
         String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         StringBuilder randomString = new StringBuilder();
 
         Random random = new Random();
         for (int i = 0; i < limit; i++) {
-            int index = random.nextInt(alphabet.length());
-            char randomCharacter = alphabet.charAt(index);
+            int index = random.nextInt(alphabet.length()); // 以字串長度作為產生亂數的限制，用以產生隨機 index (索引)
+            char randomCharacter = alphabet.charAt(index); // 以隨機產生的索引，取得隨機字元(char)
             randomString.append(randomCharacter);
         }
 
         String rtn = randomString.toString();
 
-        System.out.println("Random::  "+rtn);
+//        System.out.println("Random::  "+rtn);
         logger.debug("Random::  {}", rtn);
         return rtn;
     }
