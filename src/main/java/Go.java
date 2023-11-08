99f333d8b4b7c81a8b97cfacb9c1ee7e7c896a3d
spyspy <spyhopping101@gmail.com>
2023-11-08 21:41:19 +0800

    update (work) 2023/11/08 週三 - 21:41:18.96

--- src/main/java/Go.java
+++ src/main/java/Go.java
@@ -2,13 +2,15 @@ import com.GoTheHell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Random;
+
 public class Go {
     private static final Logger logger = LogManager.getLogger(Go.class);
 
     public static void main(String[] args) {
 
         logger.debug("Hello from Log4j 2 - V111 ============================(Start)");
-
+        String newWord = getRandomWord(1000);
         // in old days, we need to check the log level to increase performance
         /*if (logger.isDebugEnabled()) {
             logger.debug("{}", getNumber());
@@ -31,5 +33,23 @@ public class Go {
 
     static int getNumber() {
         return 5;
+    }
+
+    static String getRandomWord(int limit){
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder randomString = new StringBuilder();
+
+        Random random = new Random();
+        for (int i = 0; i < limit; i++) {
+            int index = random.nextInt(alphabet.length());
+            char randomCharacter = alphabet.charAt(index);
+            randomString.append(randomCharacter);
+        }
+
+        String rtn = randomString.toString();
+
+        System.out.println("Random::  "+rtn);
+        logger.debug("Random::  {}", rtn);
+        return rtn;
     }
 }
