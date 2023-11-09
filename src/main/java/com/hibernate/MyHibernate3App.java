1ccf260aa483390c1acd1e99bd82a57b1490ab24
spyspy <spyhopping101@gmail.com>
2023-11-09 17:14:03 +0800

    update (work) 2023/11/09 週四 - 17:13:52.22

--- src/main/java/com/hibernate/MyHibernate3App.java
+++ src/main/java/com/hibernate/MyHibernate3App.java
@@ -1,12 +1,18 @@
 package com.hibernate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class MyHibernate3App {
+    private static final Logger logger = LogManager.getLogger(MyHibernate3App.class);
+
     public static void main(String[] args) {
+        logger.info("MyHibernate3App Running.....");
         // Create a Hibernate configuration
+//        Configuration configuration = new Configuration();
         Configuration configuration = new Configuration();
         configuration.configure(); // Load hibernate.cfg.xml (assumes it's in the classpath)
 
